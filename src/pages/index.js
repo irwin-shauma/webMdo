260070c092b9a7f3d7aef27602bf84dbc7703292
irwin-shauma <irwin.shauma.rizky@gmail.com>
2023-03-27 01:27:27 +0700

    Removing unused files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,5 @@
 import Head from "next/head";
 import { Box, Container, Grid, Typography } from "@mui/material";
-import { Budget } from "../components/dashboard/budget";
-import { LatestOrders } from "../components/dashboard/latest-orders";
-import { LatestProducts } from "../components/dashboard/latest-products";
-import { Sales } from "../components/dashboard/sales";
-import { TasksProgress } from "../components/dashboard/tasks-progress";
-import { TotalCustomers } from "../components/dashboard/total-customers";
-import { TotalProfit } from "../components/dashboard/total-profit";
-import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
 import { DashboardLayout } from "../components/dashboard-layout";
 
 const Page = () => (
@@ -33,27 +25,6 @@ const Page = () => (
           </Grid>
           {/* <Grid item lg={8} md={12} xl={9} xs={12}>
             created by tsaqif
-          </Grid> */}
-          {/* <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalCustomers />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TasksProgress />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalProfit sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <Sales />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <TrafficByDevice sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <LatestProducts sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <LatestOrders />
           </Grid> */}
         </Grid>
       </Container>
--- src/pages/login.js
+++ src/pages/login.js
@@ -60,86 +60,9 @@ const Login = () => {
         }}
       >
         <Container maxWidth="sm">
-          {/* <NextLink
-            href="/"
-            passHref
-          >
-            <Button
-              component="a"
-              startIcon={<ArrowBackIcon fontSize="small" />}
-            >
-              Dashboard
-            </Button>
-          </NextLink> */}
           <AlertBox open={open} setOpen={setOpen} message={messageAlert} errorType={errorAlert} />
 
           <form onSubmit={formik.handleSubmit}>
-            {/* <Box sx={{ my: 3 }}>
-              <Typography
-                color="textPrimary"
-                variant="h4"
-              >
-                Sign in
-              </Typography>
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="body2"
-              >
-                Sign in on the internal platform
-              </Typography>
-            </Box> */}
-            {/* <Grid
-              container
-              spacing={3}
-            >
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="info"
-                  fullWidth
-                  startIcon={<FacebookIcon />}
-                  onClick={() => formik.handleSubmit()}
-                  size="large"
-                  variant="contained"
-                >
-                  Login with Facebook
-                </Button>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="error"
-                  fullWidth
-                  onClick={() => formik.handleSubmit()}
-                  size="large"
-                  startIcon={<GoogleIcon />}
-                  variant="contained"
-                >
-                  Login with Google
-                </Button>
-              </Grid>
-            </Grid> */}
-            {/* <Box
-              sx={{
-                pb: 1,
-                pt: 3
-              }}
-            >
-              <Typography
-                align="center"
-                color="textSecondary"
-                variant="body1"
-              >
-                or login with email address
-              </Typography>
-            </Box> */}
             <TextField
               error={Boolean(formik.touched.user && formik.errors.user)}
               fullWidth
@@ -178,27 +101,6 @@ const Login = () => {
                 Sign In
               </Button>
             </Box>
-            {/* <Typography
-              color="textSecondary"
-              variant="body2"
-            >
-              Don&apos;t have an account?
-              {' '}
-              <NextLink
-                href="/register"
-              >
-                <Link
-                  to="/register"
-                  variant="subtitle2"
-                  underline="hover"
-                  sx={{
-                    cursor: 'pointer'
-                  }}
-                >
-                  Sign Up
-                </Link>
-              </NextLink>
-            </Typography> */}
           </form>
         </Container>
       </Box>
